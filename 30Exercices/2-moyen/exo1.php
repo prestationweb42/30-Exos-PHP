3f60314690ee8afe99491b5f5c3828e02771db7d
danivent1965 <dani.vent@orange.fr>
2023-04-14 09:40:41 +0200

    add exo1 moyen

--- 30Exercices/2-moyen/exo1.php
+++ 30Exercices/2-moyen/exo1.php
@@ -4,33 +4,46 @@ $titre = "Exo 1 : Les tables de multiplications"; //Mettre le nom du titre de la
 
 <!-- mettre ici le code -->
 <?php
+// déclarer un tableau vide pour les lignes
 $ligne = [];
-for($j=1;$j <= 10;$j++){
+// on boucle pour générer les lignes
+for ($j = 1; $j <= 10; $j++) {
+    // on déclare un tableau vide pour les colonnes
     $colonne = [];
-    for($i=1;$i <= 10; $i++){
+    // on boucle pour générer les colonnes
+    for ($i = 1; $i <= 10; $i++) {
+        // on remplie les colonnes par les multiplications
         $colonne[] = $i * $j;
     }
+    // on remplie les lignes avec le résultat des colonnes 
     $ligne[] = $colonne;
 }
+// echo "<pre>";
+// print_r($ligne);
+// echo "</pre>";
+
 ?>
 <table class="table">
-    <?php for($j=0;$j < 10;$j++) : ?>
-    <tr <?= ($j===0) ? 'class="font-weight-bold"' : '' ?>>
-        <?php for($i=0;$i < 10; $i++) :?>
-        <td <?= ($i===0) ? 'class="font-weight-bold"' : '' ?>>
+    <?php
+    for ($j = 0; $j < 10; $j++) { ?>
+    <tr <?= ($j === 0) ? 'class="font-weight-bold"' : '' ?>>
+        <?php for ($i = 0; $i < 10; $i++) { ?>
+        <td <?= ($i === 0) ? 'class="font-weight-bold"' : '' ?>>
             <?= $ligne[$j][$i] ?>
         </td>
-        <?php endfor;?>
+        <?php } ?>
     </tr>
-    <?php endfor; ?>
+    <?php } ?>
+
 </table>
+
 
 <?php
 /************************
  * NE PAS MODIFIER
  * PERMET d INCLURE LE MENU ET LE TEMPLATE
  ************************/
-    $content = ob_get_clean();
-    // require "../../global/common/template.php";
-    require "../global/common/template.php";
+$content = ob_get_clean();
+// require "../../global/common/template.php";
+require "../global/common/template.php";
 ?>
