4fa3667b0354c06cbaea6e9ca42ec9389e035ce5
danivent1965 <dani.vent@orange.fr>
2023-04-26 08:35:10 +0200

    modify navbar

--- 30Exercices/components/navbar.php
+++ 30Exercices/components/navbar.php
@@ -1,7 +1,9 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../../../PHP-30-Exercices/index.html"><img src="../img/php-logo.png" alt="logo php" class="navbar__brand__img"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="../../../PHP-30-Exercices/index.html"><img src="../img/php-logo.png"
+                alt="logo php" class="navbar__brand__img"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse navlink__container" id="navbarSupportedContent">
@@ -9,11 +11,9 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="../../PHP-30-Exercices/index.html">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
                         03-Difficile
                     </a>
                     <ul class="dropdown-menu">
@@ -26,7 +26,8 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
                         02-Moyen
                     </a>
                     <ul class="dropdown-menu">
@@ -59,31 +60,32 @@
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
                         01-Facile
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="1-facile/exo1.php">Exo-1</a></li>
+                        <li><a class="dropdown-item" href="1-facile/exo1.php">Exo-1-Variables</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="1-facile/exo2.php">Exo-2</a></li>
+                        <li><a class="dropdown-item" href="1-facile/exo2.php">Exo-2-Var-Ternaires</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="1-facile/exo3.php">Exo-3</a></li>
+                        <li><a class="dropdown-item" href="1-facile/exo3.php">Exo-3-if()</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="1-facile/exo4.php">Exo-4</a></li>
+                        <li><a class="dropdown-item" href="1-facile/exo4.php">Exo-4-if()</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="1-facile/exo5.php">Exo-5</a></li>
+                        <li><a class="dropdown-item" href="1-facile/exo5.php">Exo-5-for()</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="1-facile/exo6.php">Exo-6</a></li>
+                        <li><a class="dropdown-item" href="1-facile/exo6.php">Exo-6-for()</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
